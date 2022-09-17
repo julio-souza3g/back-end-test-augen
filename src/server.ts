d8e97470e9e2212325eec66bd75f15508596f138
Julio Souza <julio3g@pop-os.localdomain>
2022-09-17 17:32:25 -0300

    chore: add prisma

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import 'dotenv/config'
-
-const app = express()
-
-app.listen(process.env.PORT, () => console.log(`Server running at http://localhost:${Number(process.env.PORT)}`))
-
-export default app
