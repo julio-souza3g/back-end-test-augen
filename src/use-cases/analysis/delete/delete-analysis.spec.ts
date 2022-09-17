b084da1b061637536e7b95af3d3358188179a70e
Julio Souza <julio3g@pop-os.localdomain>
2022-09-17 12:22:18 -0300

    test: add delete analysis use case

---
+++ src/use-cases/analysis/delete/delete-analysis.spec.ts
@@ -0,0 +1,41 @@
+import { InMemoryAnalysisRepository } from '../../../repositories/in-memory/in-memory-analysis-repository'
+import { CreateAnalysis } from '../create/create-analysis'
+import { GetAnalysisById } from '../get-by-id/get-analysis-by-id'
+import { DeleteAnalysis } from './delete-analysis'
+
+interface SutTypes {
+  createAnalysis: CreateAnalysis
+  getAnalysisById: GetAnalysisById
+  sut: DeleteAnalysis
+}
+
+const makeSut = (): SutTypes => {
+  const inMemoryAnalysisRepository = new InMemoryAnalysisRepository()
+  const createAnalysis = new CreateAnalysis(inMemoryAnalysisRepository)
+  const getAnalysisById = new GetAnalysisById(inMemoryAnalysisRepository)
+  const sut = new DeleteAnalysis(inMemoryAnalysisRepository)
+  return {
+    createAnalysis,
+    getAnalysisById,
+    sut
+  }
+}
+
+describe('DeleteAnalysis', () => {
+  it('should throw if analysis does not exist', async () => {
+    const { sut } = makeSut()
+    await expect(sut.execute('invalid-id')).rejects.toThrow('Analysis not found')
+  })
+
+  it('should be able delete analysis', async () => {
+    const { createAnalysis, getAnalysisById, sut } = makeSut()
+    const analysis = await createAnalysis.execute({
+      phLevel: 7.5,
+      chlorineLevel: 0.5,
+      fluorideLevel: 0.5,
+      flowRate: 0.5
+    })
+    await sut.execute(analysis.id)
+    await expect(getAnalysisById.execute(analysis.id)).rejects.toThrow()
+  })
+})
