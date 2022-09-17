9f61a5462fbf2ec745d7080d58021f42c4e1cd31
Julio Souza <julio3g@pop-os.localdomain>
2022-09-17 15:49:26 -0300

    test: add delete equipment use case

---
+++ src/use-cases/equipment/delete/delete-equipment.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryEquipmentRepository } from '../../../repositories/in-memory/in-memory-equipment-repository'
+import { CreateEquipment } from '../create/create-equipment'
+import { GetEquipmentById } from '../get-by-id/get-equipment-by-id'
+import { DeleteEquipment } from './delete-equipment'
+
+interface SutTypes {
+  createEquipment: CreateEquipment
+  getEquipmentById: GetEquipmentById
+  sut: DeleteEquipment
+}
+
+const makeSut = (): SutTypes => {
+  const inMemoryEquipmentRepository = new InMemoryEquipmentRepository()
+  const createEquipment = new CreateEquipment(inMemoryEquipmentRepository)
+  const getEquipmentById = new GetEquipmentById(inMemoryEquipmentRepository)
+  const sut = new DeleteEquipment(inMemoryEquipmentRepository)
+  return {
+    createEquipment,
+    getEquipmentById,
+    sut
+  }
+}
+
+describe('DeleteEquipment', () => {
+  it('should be able delete an equipment', async () => {
+    const { createEquipment, getEquipmentById, sut } = makeSut()
+    const equipment = await createEquipment.execute({
+      name: 'Equipment name',
+      description: 'Equipment description',
+      cityId: 'city-id'
+    })
+    await sut.execute(equipment.id)
+    await expect(getEquipmentById.execute(equipment.id)).rejects.toThrow()
+  })
+
+  it('should be able throw if equipment not found', async () => {
+    const { sut } = makeSut()
+    await expect(sut.execute('invalid_id')).rejects.toThrow()
+  })
+})
