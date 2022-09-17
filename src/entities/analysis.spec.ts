467eed958086ac007463228361801af47a739394
Julio Souza <julio3g@pop-os.localdomain>
2022-09-17 01:29:45 -0300

    test: ensure validations on create analysis entity

---
+++ src/entities/analysis.spec.ts
@@ -0,0 +1,48 @@
+import { Analysis } from './analysis'
+
+describe('Create an analysis', () => {
+  it('should be able create an analysis', () => {
+    const analysis = new Analysis({
+      phLevel: 7.5,
+      chlorineLevel: 0.5,
+      fluorideLevel: 0.5,
+      flowRate: 0.5
+    })
+
+    expect(analysis).toBeInstanceOf(Analysis)
+    expect(analysis.phLevel).toEqual(7.5)
+  })
+
+  it('should not be able create an analysis with flow rate to equal zero', () => {
+    expect(() => {
+      return new Analysis({
+        phLevel: 7.5,
+        chlorineLevel: 0.5,
+        fluorideLevel: 0.5,
+        flowRate: 0
+      })
+    }).toThrow()
+  })
+
+  it('should not be able create an analysis with chlorine level to greater than 100', () => {
+    expect(() => {
+      return new Analysis({
+        phLevel: 7.5,
+        chlorineLevel: 101,
+        fluorideLevel: 0.5,
+        flowRate: 0.5
+      })
+    }).toThrow()
+  })
+
+  it('should not be able create an analysis with fluoride level to greater than 100', () => {
+    expect(() => {
+      return new Analysis({
+        phLevel: 7.5,
+        chlorineLevel: 0.5,
+        fluorideLevel: 101,
+        flowRate: 0.5
+      })
+    }).toThrow()
+  })
+})
