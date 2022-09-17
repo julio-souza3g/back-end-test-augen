c6afefd055c8dc6c531d58b0fa4dd95dc86913a7
Julio Souza <julio3g@pop-os.localdomain>
2022-09-17 12:22:53 -0300

    test: add get all analysis use case

---
+++ src/use-cases/analysis/get-all/get-all-analysis.spec.ts
@@ -0,0 +1,33 @@
+import { InMemoryAnalysisRepository } from '../../../repositories/in-memory/in-memory-analysis-repository'
+import { CreateAnalysis } from '../create/create-analysis'
+import { GetAllAnalysis } from './get-all-analysis'
+
+interface SutTypes {
+  createAnalysis: CreateAnalysis
+  sut: GetAllAnalysis
+}
+
+const makeSut = (): SutTypes => {
+  const inMemoryAnalysisRepository = new InMemoryAnalysisRepository()
+  const createAnalysis = new CreateAnalysis(inMemoryAnalysisRepository)
+  const sut = new GetAllAnalysis(inMemoryAnalysisRepository)
+  return {
+    createAnalysis,
+    sut
+  }
+}
+
+describe('GetAllAnalysis', () => {
+  it('should be able get all analysis', async () => {
+    const { createAnalysis, sut } = makeSut()
+    await createAnalysis.execute({
+      phLevel: 7.5,
+      chlorineLevel: 0.5,
+      fluorideLevel: 0.5,
+      flowRate: 0.5
+    })
+    const analysis = await sut.execute()
+    expect(analysis).toBeInstanceOf(Array)
+    expect(analysis.length).toBeGreaterThan(0)
+  })
+})
