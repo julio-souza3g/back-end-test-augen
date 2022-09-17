41bbd602ce08edabe98dd7a8c517010f589af9f1
Julio Souza <julio3g@pop-os.localdomain>
2022-09-17 12:22:05 -0300

    feat: add delete analysis use case

--- src/use-cases/analysis/delete/delete-analysis.ts
+++ src/use-cases/analysis/delete/delete-analysis.ts
@@ -1,16 +1,15 @@
-import { Analysis } from '../../entities/analysis'
-import { AnalysisRepository } from '../../repositories/analysis-repository'
+import { AnalysisRepository } from '../../../repositories/analysis-repository'
 
-export class GetAnalysisById {
+export class DeleteAnalysis {
   constructor (
     private readonly analysisRepository: AnalysisRepository
   ) {}
 
-  async execute (id: string): Promise<Analysis> {
+  async execute (id: string): Promise<void> {
     const analysis = await this.analysisRepository.findById(id)
     if (analysis === null) {
       throw new Error('Analysis not found')
     }
-    return analysis
+    await this.analysisRepository.delete(id)
   }
 }
